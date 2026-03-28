cedd47b2255cfba24a964c50012ae750db687b68
Sudeep Kalapati <sudeepkalapati@outlook.com>
2026-03-28 16:20:14 +0530

    Initia commit - Project project

---
+++ SudeepProject/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SudeepProject.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
